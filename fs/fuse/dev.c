846356a2318312cfc0bd26aea0135470fb8297fd
Miklos Szeredi <mszeredi@redhat.com>
2020-02-13 09:16:07 +0100

    BACKPORT: fuse: fix stack use after return
    Normal, synchronous requests will have their args allocated on the stack.
    After the FR_FINISHED bit is set by receiving the reply from the userspace
    fuse server, the originating task may return and reuse the stack frame,
    resulting in an Oops if the args structure is dereferenced.
    
    Fix by setting a flag in the request itself upon initializing, indicating
    whether it has an asynchronous ->end() callback.
    
    Reported-by: Kyle Sanderson <kyle.leet@gmail.com>
    Reported-by: Michael Stapelberg <michael+lkml@stapelberg.ch>
    Fixes: 2b319d1f6f92 ("fuse: don't dereference req->args on finished request")
    Cc: <stable@vger.kernel.org> # v5.4
    Tested-by: Michael Stapelberg <michael+lkml@stapelberg.ch>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: kawaaii <kawaaii@nocturn9x.space>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -264,7 +264,6 @@ static void flush_bg_queue(struct fuse_conn *fc)
 void fuse_request_end(struct fuse_conn *fc, struct fuse_req *req)
 {
 	struct fuse_iqueue *fiq = &fc->iq;
-	bool async = req->args->end;
 
 	if (test_and_set_bit(FR_FINISHED, &req->flags))
 		goto put_request;
@@ -311,7 +310,7 @@ void fuse_request_end(struct fuse_conn *fc, struct fuse_req *req)
 		wake_up(&req->waitq);
 	}
 
-	if (async)
+	if (test_bit(FR_ASYNC, &req->flags))
 		req->args->end(fc, req->args, req->out.h.error);
 put_request:
 	fuse_put_request(fc, req);
@@ -459,6 +458,8 @@ static void fuse_args_to_req(struct fuse_req *req, struct fuse_args *args)
 	req->in.h.opcode = args->opcode;
 	req->in.h.nodeid = args->nodeid;
 	req->args = args;
+	if (args->end)
+		__set_bit(FR_ASYNC, &req->flags);
 }
 
 ssize_t fuse_simple_request(struct fuse_conn *fc, struct fuse_args *args)
