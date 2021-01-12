6458d493de42306f04aa18e86b2a19fc50ff3c02
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-12 00:21:30 -0800

    ASoC: codecs: Fix uninitialized variable usage
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: kawaaii <kawaaii@nocturn9x.space>

--- techpack/audio/asoc/codecs/wcd_cpe_core.c
+++ techpack/audio/asoc/codecs/wcd_cpe_core.c
@@ -3598,6 +3598,7 @@ static int wcd_cpe_lsm_eob(
 	int ret = 0;
 	struct cmi_hdr lab_eob;
 
+	memset(&lab_eob, 0, sizeof(lab_eob));
 	if (fill_lsm_cmd_header_v0_inband(&lab_eob, session->id,
 		0, CPE_LSM_SESSION_CMD_EOB)) {
 		return -EINVAL;
@@ -4154,6 +4155,7 @@ static int wcd_cpe_send_afe_cal(void *core_handle,
 		struct cmi_hdr *hdr = &(obm_msg.hdr);
 		struct cmi_obm *pld = &(obm_msg.pld);
 
+		memset(&obm_msg, 0, sizeof(obm_msg));
 		rc = wcd_cpe_afe_shmem_alloc(core, port_d,
 					afe_cal->cal_data.size);
 		if (rc) {
