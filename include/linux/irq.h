22183eb3099a5e214f3578b76a6142c913431ddf
Sultan Alsawaf <sultan@kerneltoast.com>
2021-09-15 22:12:16 +0530

    kernel: Add bi-cluster API to affine IRQs and kthreads to fast CPUs
     On devices with a CPU that contains heterogeneous cores (e.g.,
     big.LITTLE), it can be beneficial to place some performance-critical
     IRQs and kthreads onto the fast CPU clusters in order to improve
     performance.
    
     This commit adds the following APIs:
      -kthread_run_perf_critical() to create and start a perf-critical kthread
        and affine it to the given performance CPUs
      -irq_set_perf_affinity() to mark an active IRQ as perf-critical and
        affine it to the given performance CPUs
      -IRQF_PERF_AFFINE to schedule an IRQ and any
        threads it may have onto the given performance CPUs
      -PF_PERF_CRITICAL to mark a process (mainly a kthread) as performance
        critical (this is used by kthread_run_perf_critical())
    
     In order to accommodate this new API, the following changes are made:
      -Performance-critical IRQs are distributed evenly among online CPUs
        available in the given performance CPU mask
      -Performance-critical IRQs have their affinities reaffined upon exit
        from suspend (since the affinities are broken when non-boot CPUs are
        disabled)
      -Performance-critical IRQs and their threads have their affinities reset
        upon entering suspend, so that upon immediate suspend exit (when only
        the boot CPU is online), interrupts can be processed and interrupt
        threads can be scheduled onto an online CPU (otherwise we'd hit a
        kernel BUG)
      -__set_cpus_allowed_ptr() is modified to enforce a performance-critical
        kthread's affinity
      -Perf-critical IRQs aren't allowed to have their affinity changed by
        userspace
    
    [ Divyanshu-Modi: Adapt for sdm660 ]
    
    Co-authored-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: kawaaii <kawaaii@nocturn9x.space>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -212,6 +212,7 @@ struct irq_data {
  *				  required
  * IRQD_AFFINITY_ON_ACTIVATE	- Affinity is set on activation. Don't call
  *				  irq_chip::irq_set_affinity() when deactivated.
+ * IRQD_PERF_CRITICAL		- IRQ is performance-critical
  */
 enum {
 	IRQD_TRIGGER_MASK		= 0xf,
@@ -235,6 +236,7 @@ enum {
 	IRQD_DEFAULT_TRIGGER_SET	= (1 << 25),
 	IRQD_CAN_RESERVE		= (1 << 26),
 	IRQD_MSI_NOMASK_QUIRK		= (1 << 27),
+	IRQD_PERF_CRITICAL		= (1 << 28),
 	IRQD_AFFINITY_ON_ACTIVATE	= (1 << 29),
 };
 
