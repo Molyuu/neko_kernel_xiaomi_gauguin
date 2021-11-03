32cd7eb5be4610484710590ee73bc613339b48a1
Lau <laststandrighthere@gmail.com>
2021-11-03 23:08:08 +0530

    crypto: qcedev: Fix misleading identation
    Fixes the following warning by GCC:
    
    ../drivers/crypto/msm/qcedev.c: In function 'qcedev_ioctl':
    ../drivers/crypto/msm/qcedev.c:1928:17: warning: this 'if' clause does not guard... [-Wmisleading-indentation]
     1928 |                 if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req,
          |                 ^~
    ../drivers/crypto/msm/qcedev.c:1931:25: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'if'
     1931 |                         goto exit_free_qcedev_areq;
          |                         ^~~~
    ../drivers/crypto/msm/qcedev.c:2020:17: warning: this 'if' clause does not guard... [-Wmisleading-indentation]
     2020 |                 if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req,
          |                 ^~
    ../drivers/crypto/msm/qcedev.c:2023:25: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'if'
     2023 |                         goto exit_free_qcedev_areq;
          |                         ^~~~
    
    [ Tashar02: Add the warn log ]
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: kawaaii <kawaaii@nocturn9x.space>

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -1876,8 +1876,7 @@ static inline long qcedev_ioctl(struct file *file,
 			err = -ENOTTY;
 			goto exit_free_qcedev_areq;
 		}
-	case QCEDEV_IOCTL_SHA_UPDATE_REQ:
-		{
+	case QCEDEV_IOCTL_SHA_UPDATE_REQ: {
 		struct scatterlist sg_src;
 
 		if (copy_from_user(&qcedev_areq->sha_op_req,
@@ -1926,10 +1925,11 @@ static inline long qcedev_ioctl(struct file *file,
 				handle->sha_ctxt.diglen);
 		mutex_unlock(&hash_access_lock);
 		if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req,
-					sizeof(struct qcedev_sha_op_req)))
+					sizeof(struct qcedev_sha_op_req))) {
 			err = -EFAULT;
 			goto exit_free_qcedev_areq;
 		}
+	}
 		break;
 
 	case QCEDEV_IOCTL_SHA_FINAL_REQ:
@@ -1977,8 +1977,7 @@ static inline long qcedev_ioctl(struct file *file,
 		handle->sha_ctxt.init_done = false;
 		break;
 
-	case QCEDEV_IOCTL_GET_SHA_REQ:
-		{
+	case QCEDEV_IOCTL_GET_SHA_REQ: {
 		struct scatterlist sg_src;
 
 		if (copy_from_user(&qcedev_areq->sha_op_req,
@@ -2018,10 +2017,11 @@ static inline long qcedev_ioctl(struct file *file,
 				handle->sha_ctxt.diglen);
 		mutex_unlock(&hash_access_lock);
 		if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req,
-					sizeof(struct qcedev_sha_op_req)))
+					sizeof(struct qcedev_sha_op_req))) {
 			err = -EFAULT;
 			goto exit_free_qcedev_areq;
 		}
+	}
 		break;
 
 	case QCEDEV_IOCTL_MAP_BUF_REQ:
