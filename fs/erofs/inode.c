aeed667cf0e720495dd6ebe1726e41d35bef54ae
Gao Xiang <hsiangkao@linux.alibaba.com>
2022-05-07 03:46:11 +0800

    Merge branch 'linux-4.19.y-erofs' of ssh://github.com/asuka-mio/erofs-unstable into dev

--- fs/erofs/inode.c
+++ fs/erofs/inode.c
@@ -7,13 +7,8 @@
 
 #include <trace/events/erofs.h>
 
-/*
- * if inode is successfully read, return its inode page (or sometimes
- * the inode payload page if it's an extended inode) in order to fill
- * inline data if possible.
- */
-static struct page *erofs_read_inode(struct inode *inode,
-				     unsigned int *ofs)
+static void *erofs_read_inode(struct erofs_buf *buf,
+			      struct inode *inode, unsigned int *ofs)
 {
 	struct super_block *sb = inode->i_sb;
 	struct erofs_sb_info *sbi = EROFS_SB(sb);
