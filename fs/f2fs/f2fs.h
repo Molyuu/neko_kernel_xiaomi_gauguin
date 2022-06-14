6df61088afb33bc3f0c58cb85166edb0d8fa2d4d
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-06-14 19:54:30 +0600

    f2fs: Conditionally set timeout for uncongestion
    Use if/else statements to condtionally set the uncongestion
    timeout based on the scheduler tick rate that is being used.
    
    Use the suggested values from the description of commit [1].
    
    [1]: https://github.com/projects-nexus/nexus_kernel_xiaomi_gauguin/commit/2e5e87d48936d280770ceef80df018880e86c1f3
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: kawaaii <kawaaii@nocturn9x.space>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -576,8 +576,16 @@ enum {
 /* congestion wait timeout value, default: 20ms */
 #define	DEFAULT_IO_TIMEOUT	(msecs_to_jiffies(20))
 
-/* congestion wait timeout value, default: 6ms */
-#define	DEFAULT_IO_TIMEOUT	(msecs_to_jiffies(6))
+/* congestion wait timeout value based on the scheduler tick rate*/
+#if (CONFIG_HZ < 100)
+#define	DEFAULT_IO_TIMEOUT	(msecs_to_jiffies(20)) /* 20ms */
+#elif CONFIG_HZ_100
+#define	DEFAULT_IO_TIMEOUT	(msecs_to_jiffies(10)) /* 10ms */
+#elif CONFIG_HZ_250
+#define	DEFAULT_IO_TIMEOUT	(msecs_to_jiffies(8)) /* 8ms */
+#elif (CONFIG_HZ > 250)
+#define	DEFAULT_IO_TIMEOUT	(msecs_to_jiffies(6)) /* 6ms */
+#endif
 
 /* maximum retry quota flush count */
 #define DEFAULT_RETRY_QUOTA_FLUSH_COUNT		8
