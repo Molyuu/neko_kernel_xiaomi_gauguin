cc72b9a2ce0d641da00cd93f6b9d27a38ac13b32
kawaaii <kawaaii@nocturn9x.space>
2022-07-02 08:26:03 +0000

    buid.sh: Update
    Signed-off-by: kawaaii <kawaaii@nocturn9x.space>

--- build.sh
+++ build.sh
@@ -36,8 +36,15 @@ DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
 TANGGAL=$(date +"%F%S")
 
 # Compiler and Build Information
-TOOLCHAIN=atomx # List ( gcc = eva | aospa | nexus9 | nexus12 ) (clang = nexus | aosp | sdclang | proton | atomx )
+TOOLCHAIN=atomx # List ( gcc = eva | nexus9 | nexus12 ) (clang = nexus | aosp | sdclang | proton | atomx )
 LINKER=ld.lld # List ( ld.lld | ld.bfd | ld.gold | ld )
+
+if [[ "$TOOLCHAIN" == "eva" || "$TOOLCHAIN" == "nexus9" || "$TOOLCHAIN" == "nexus12" ]]; then
+       COMPILER=gcc
+elif [[ "$TOOLCHAIN" == "nexus" || "$TOOLCHAIN" == "proton" || "$TOOLCHAIN" == "aosp" || "$TOOLCHAIN" == "sdclang" || "$TOOLCHAIN" == "atomx" ]]; then
+       COMPILER=clang
+fi
+
 VERBOSE=0
 ZIPNAME=NexusKernel
 FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-${TANGGAL}.zip
@@ -47,7 +54,7 @@ FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-${TANGGAL}.zip
            if [ "$CIRCLECI" ]; then
                   export CI_BRANCH=${CIRCLE_BRANCH}
            elif [ "$DRONE" ]; then
-		  export CI_BRANCH=${DRONE_BRANCH}
+                export CI_BRANCH=${DRONE_BRANCH}
            elif [ "$CIRRUS_CI" ]; then
                   export CI_BRANCH=${CIRRUS_BRANCH}
            fi
@@ -56,20 +63,20 @@ FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-${TANGGAL}.zip
 ##----------------------------------------------------------##
 ## Telegram Bot Integration
 function post_msg() {
-	curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-	-d chat_id="$chat_id" \
-	-d "disable_web_page_preview=true" \
-	-d "parse_mode=html" \
-	-d text="$1"
-	}
+       curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+       -d chat_id="$chat_id" \
+       -d "disable_web_page_preview=true" \
+       -d "parse_mode=html" \
+       -d text="$1"
+       }
 
 function push() {
-	curl -F document=@$1 "https://api.telegram.org/bot$token/sendDocument" \
-	-F chat_id="$chat_id" \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$2"
-	}
+       curl -F document=@$1 "https://api.telegram.org/bot$token/sendDocument" \
+       -F chat_id="$chat_id" \
+       -F "disable_web_page_preview=true" \
+       -F "parse_mode=html" \
+       -F caption="$2"
+       }
 ##----------------------------------------------------------------##
 ## Get Dependencies
 function clone() {
@@ -77,9 +84,6 @@ function clone() {
 if [[ $TOOLCHAIN == "eva" ]]; then
        git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 gcc64
        git clone --depth=1 https://github.com/mvaisakh/gcc-arm gcc32
-elif [[ $TOOLCHAIN == "aospa" ]]; then
-       git clone --depth=1 https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_aarch64_aarch64-elf gcc64
-       git clone --depth=1 https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_arm_arm-eabi gcc32
 elif [[ $TOOLCHAIN == "nexus9" ]]; then
        git clone --depth=1 https://github.com/reaPeR1010/arm64-gcc -b gcc-9 gcc64
        git clone --depth=1 https://github.com/reaPeR1010/arm32-gcc -b gcc-9 gcc32
@@ -110,7 +114,7 @@ fi
 git clone --depth=1 https://github.com/reaPeR1010/AnyKernel3 AK3
 
 # Set PATH
-if [[ "$TOOLCHAIN" == "eva" || "$TOOLCHAIN" == "aospa" || "$TOOLCHAIN" == "nexus9" || "$TOOLCHAIN" == "nexus12" ]]; then
+if [[ "$COMPILER" == "gcc" ]]; then
        PATH="${KERNEL_DIR}/gcc64/bin/:${KERNEL_DIR}/gcc32/bin/:/usr/bin:${PATH}"
 elif [[ "$TOOLCHAIN" == "nexus" || "$TOOLCHAIN" == "proton" || "$TOOLCHAIN" == "atomx" ]]; then
        PATH="${KERNEL_DIR}/clang/bin:${PATH}"
@@ -119,9 +123,9 @@ elif [[ "$TOOLCHAIN" == "aosp" || "$TOOLCHAIN" == "sdclang" ]]; then
 fi
 
 # Export KBUILD_COMPILER_STRING
-if [[ "$TOOLCHAIN" == "eva" || "$TOOLCHAIN" == "aospa" || "$TOOLCHAIN" == "nexus9" || "$TOOLCHAIN" == "nexus12" ]]; then
+if [[ "$COMPILER" == "gcc" ]]; then
        export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/gcc64/bin/aarch64-elf-gcc --version | head -n 1)
-elif [[ "$TOOLCHAIN" == "nexus" || "$TOOLCHAIN" == "proton" || "$TOOLCHAIN" == "aosp" || "$TOOLCHAIN" == "sdclang" || "$TOOLCHAIN" == "atomx" ]]; then
+elif [[ "$COMPILER" == "clang" ]]; then
        export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 fi
 }
@@ -133,12 +137,16 @@ START=$(date +"%s")
 post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Kolkata date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Version : </b><code>$VERSION</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Linker : </b><code>$LINKER</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>"
 
 # Generate .config
+if [[ $COMPILER == "gcc" ]]; then
+make O=out ARCH=arm64 ${DEFCONFIG}
+elif [[ $COMPILER == "clang" ]]; then
 make O=out LLVM=1 ARCH=arm64 ${DEFCONFIG}
+fi
 
 # Start Compilation
-if [[ "$TOOLCHAIN" == "eva" || "$TOOLCHAIN" == "aospa" || "$TOOLCHAIN" == "nexus9" || "$TOOLCHAIN" == "nexus12" ]]; then
+if [[ "$COMPILER" == "gcc" ]]; then
      MAKE+=( HOSTLD=ld.lld CROSS_COMPILE_COMPAT=arm-eabi- CROSS_COMPILE=aarch64-elf- LD=aarch64-elf-${LINKER} AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip OBJSIZE=llvm-size )
-elif [[ "$TOOLCHAIN" == "nexus" || "$TOOLCHAIN" == "proton" || "$TOOLCHAIN" == "aosp" || "$TOOLCHAIN" == "sdclang" || "$TOOLCHAIN" == "atomx" ]]; then
+elif [[ "$COMPILER" == "clang" ]]; then
      MAKE+=( LLVM=1 LLVM_IAS=1 HOSTLD=ld.lld HOSTAR=llvm-ar )
      if [[ "$TOOLCHAIN" == "aosp" || "$TOOLCHAIN" == "sdclang" ]]; then
      MAKE+=( CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_COMPAT=arm-linux-androideabi- )
@@ -150,13 +158,13 @@ fi
 make -kj$PROCS ARCH=arm64 O=out V=$VERBOSE "${MAKE[@]}" 2>&1 | tee error.log
 
 # Verify Files
-	if ! [ -a "$IMAGE" ];
-	   then
-	       push "error.log" "Build Throws Errors"
-	       exit 1
-	   else
-      	       post_msg " Kernel Compilation Finished. Started Zipping "
-	fi
+       if ! [ -a "$IMAGE" ];
+          then
+              push "error.log" "Build Throws Errors"
+              exit 1
+          else
+              post_msg " Kernel Compilation Finished. Started Zipping "
+       fi
 }
 ##----------------------------------------------------------------##
 function zipping() {
